70f9adedc6bbed3a5e77016a351874b41cbcdf19
HaoES <hamza.essamaali@gmail.com>
2024-01-01 21:41:23 +0100

    added positonal encoding

--- model.py
+++ model.py
@@ -11,4 +11,32 @@ class InputEmbeddings(nn.Module):
         self.embedding = nn.Embedding(vocab_size, d_model)
 
     def forward(self, x):
-        return self.embedding(x) * math.sqrt(d_model)
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # creating a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # creating a vector of shape (seq_len,1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        denom = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
+        # applying sin to even positons
+        pe[:, 0::2] = torch.sin(position * denom)
+        pe[:, 1::2] = torch.cos(position * denom)
+
+        # add a batch dimension
+        pe = pe.unsqueeze(0)  # (1,seq_len,d_model)
+
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, : x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
