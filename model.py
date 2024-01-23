10c44dcae3847a353501392ce618f9a6f2efe5ef
HaoES <hamza.essamaali@gmail.com>
2024-01-23 13:24:17 +0100

    added decoder

--- model.py
+++ model.py
@@ -166,3 +166,45 @@ class Encoder(nn.Module):
         for layer in self.layers:
             x = layer(x, mask)
         return self.norm(x)
+
+
+class DecoderBlock(nn.Module):
+    def __init__(
+        self,
+        self_attention_block: MultiHeadAttentionBlock,
+        cross_attention_block: MultiHeadAttentionBlock,
+        feed_forward_block: FeedForwardBlock,
+        dropout: float,
+    ):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.Module(
+            [ResidualConnection(dropout) for _ in range(3)]
+        )
+
+    def forward(self, x, encoder_output, src_mask, trgt_mask):
+        x = self.residual_connections[0](
+            x, lambda x: self.self_attention_block(x, x, x, trgt_mask)
+        )
+        x = self.residual_connections[1](
+            x,
+            lambda x: self.cross_attention_block(
+                x, encoder_output, encoder_output, src_mask
+            ),
+        )
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, encoder_output, src_mask, trgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, trgt_mask)
+        return self.norm(x)
